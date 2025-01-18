336826dcba6b63086ce6d068c61da74c75238f38
= <=>
2025-01-18 16:15:51 +0530

    starting update on anime

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 var h5timer = document.querySelector('#prog1');
 var grow = 0;
-
-
-var tl = gsap.timeline();
-tl.from('.line h1', {
-    y: 130,
-    stagger: 0.25,
-    duration: 0.6,
-    delay: 0.5,
-})
-tl.from('#line1-part1, .line h2',{
+gsap.from('#line1-part1',{
+    delay: 1,
     opacity: 0,
     onStart: function(){
         if(grow<=100){
@@ -22,13 +14,31 @@ tl.from('#line1-part1, .line h2',{
         }
     }
 })
+
+var tl = gsap.timeline();
+tl.from('.line h1', {
+    y: 130,
+    stagger: 0.3,
+    duration: 0.6,
+    delay: 0.5,
+})
+tl.to('.line h2', {
+    animationName: 'anime',
+    opacity: 1,
+    duration: 5, // Max duration
+    onUpdate: () => {
+        if (grow >= 100) {
+            gsap.set('.line h2', { animationName: '' }); // Reset animationName
+        }
+    }
+});
 tl.from('#loader-msg', {
     opacity: 0,
 })
 tl.to('#loader', {
     opacity: 0,
     duration: 0.4,
-    delay: 3,
+    delay: 2,
 })
 tl.from('#page1', {
     y: 1200,
